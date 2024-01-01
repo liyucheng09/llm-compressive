7c3d44ac8f9592764b1cd18deefaec461000413a
yucheng lab <liyucheng_2@qq.com>
2024-01-01 13:54:22 +0000

    bug fix

--- tests/test_png.py
+++ tests/test_png.py
@@ -5,7 +5,7 @@ text = ' '.join(['pytorch'] * 1000)
 byte_stream = text.encode('utf-8')
 print(len(byte_stream))
 
-w = png.Writer(len(text), 1, greyscale=True, bitdepth=8)
+w = png.Writer(len(byte_stream), 1, greyscale=True, bitdepth=8)
 with open('compressed.png', 'wb') as f:
     w.write(f, [byte_stream])
 print(os.path.getsize('compressed.png'))
